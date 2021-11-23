c08c00d12e94b57017822289efebacfb42c28d95
Wjs-SmallCoder <2367809849@qq.com>
2021-11-23 21:56:11 +0800

    axios end

--- server.js
+++ server.js
@@ -2,8 +2,25 @@ const express = require('express')
 
 const app = express();
 
+// 允许跨域（需要插件）
+// app.use(cors())
+// 解析urlencoded post 请求体参数
+app.use(express.urlencoded())
+// 解析json 请求体参数 
+// app.use(express.json())
+
+const posts = [
+    {
+        name: "1",
+        age: "1"
+    },{
+        name: "2",
+        age: "2"
+    }
+]
+
 app.get('/',function(request,response) {
-    response.sendFile(__dirname + '/text/axios_拦截器取消请求.html')
+    response.sendFile(__dirname + '/text/axios_二次封装.html')
 })
 
 app.get('/getProducts1',function(request,response) {
@@ -24,6 +41,21 @@ app.get('/getProducts2',function(request,response) {
                 name: 2
             }
         ])
+    },1000 + (Math.random() * 6))
+})
+
+app.get('/get',(request,response) => {
+    setTimeout(() => {
+        response.send(posts)
+    },1000 + (Math.random() * 6))
+})
+
+app.post('/get',(request,response) => {
+    setTimeout(() => {
+        const {name,age} = request.body
+        const post = {name,age}
+        posts.push(post)
+        response.send({date: post})
     },1000 + (Math.random() * 6))
 })
 
